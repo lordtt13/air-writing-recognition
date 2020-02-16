55a1d5e60272e80514e688f87580d3eb30640f34
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-16 23:00:52 +0530

    Training executed

--- train.py
+++ train.py
@@ -25,6 +25,11 @@ image_y = 32
 Y = to_categorical(Y)
 X = np.reshape(X, (X.shape[0], image_x, image_y, 1))
 
+# Only uncomment if you want to retrain
+
+# Vanilla model has optimal speed whereas separable conv model has optimal accuracy
+# Introducing additional complexity causes mode collapse
+
 """
 model_new = vanilla_conv(image_x, image_y)
 
@@ -38,10 +43,11 @@ model_sc = sc_conv(image_x, image_y)
 model_sc.fit(X, Y, batch_size = 32, epochs = 10, validation_split =  0.25)
 
 model_sc.save("sc_conv.h5")
-"""
+
 
 model_complex = sc_conv_complex(image_x, image_y)
 
 model_complex.fit(X, Y, batch_size = 32, epochs = 10, validation_split =  0.25)
 
 model_complex.save("sc_conv_complex.h5")
+"""
