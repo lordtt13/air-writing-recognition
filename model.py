6521c719938ba6cf61787cd936fa610269298a91
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-16 22:24:53 +0530

    Vanilla SC2D Model Add

--- model.py
+++ model.py
@@ -9,9 +9,10 @@ import numpy as np
 import pandas as pd
 import tensorflow.keras.backend as K
 
-from tensorflow.keras.layers import Input, Dense, Activation, BatchNormalization, Flatten, Conv2D, SeparableConv2D
+from tensorflow.keras.layers import Input, Dense, Activation, BatchNormalization, Flatten, Conv2D, SeparableConv2D, Concatenate
 from tensorflow.keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
 from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam, SGD
 
 
 def vanilla_conv(image_x,image_y):
@@ -24,5 +25,23 @@ def vanilla_conv(image_x,image_y):
     model.add(Flatten())
     model.add(Dense(num_of_classes, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.summary()
+    
+    return model
+
+def sc_conv(image_x,image_y):
+    num_of_classes = 37
+    inp_ = Input((image_x, image_y))
+    x = SeparableConv2D(filters = 64, kernel_size = (3, 3), activation='relu', padding = 'same')(inp_)
+    x = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='same')(x)
+    x = SeparableConv2D(filters = 32, kernel_size = (3, 3), activation='relu', padding = 'same')(x)
+    x = MaxPooling2D(pool_size=(5, 5), strides=(5, 5), padding='same')(x)
+    x = Flatten()(x)
+    out = Dense(num_of_classes, activation='softmax')(x)
+    
+    model = Model(inp_, out)
+    
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
+
